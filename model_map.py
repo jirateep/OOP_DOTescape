1ebe401f67a003db656426b424af5b2d6689a09b
jirateep <charoenshop@gmail.com>
2016-12-07 22:48:27 +0700

    **complete generate map

--- model_map.py
+++ model_map.py
@@ -12,57 +12,82 @@ class Map:
     map = 0
 
     def __init__(self):
-        self.genarate_map()
+        self.generate_map()
         self.print_map()
 
-    def genarate_map(self):
+    def generate_map(self):
         self.map = [[[0 for col in range(4)]for col in range(self.col)] for row in range(self.row)]
         #self.map = [[[0]*4]*self.col]*self.row
         #print(self.map)
         for i in range(0,self.row):
             for j in  range(0,self.col):
-                if i == 0:
-                    #print("hi3")
-                    self.map[i][j][self.DOOR_UP] = -1
-                elif self.map[i-1][j][self.DOOR_DOWN] != 0:
-                    #print("hi2")
-                    self.map[i][j][self.DOOR_UP] = self.map[i-1][j][self.DOOR_DOWN]
-                else:
-                    #print("hi")
-                    if(not self.three_close_door(i,j)):
-                        self.map[i][j][self.DOOR_UP] = self.random_status();
-                    else:
-                        self.map[i][j][self.DOOR_UP] = 1;
+                sequence = self.generate_seq()
+                for step in sequence:
+                    if step == self.DOOR_UP:
+                        self.generate_up_door(i,j)
+                    if step == self.DOOR_DOWN:
+                        self.generate_down_door(i,j)
+                    if step == self.DOOR_LEFT:
+                        self.generate_left_door(i,j)
+                    if step == self.DOOR_RIGHT:
+                        self.generate_right_door(i,j)
 
-                if i == self.row - 1:
-                    self.map[i][j][self.DOOR_DOWN] = -1
-                elif self.map[i+1][j][self.DOOR_UP] != 0:
-                    self.map[i][j][self.DOOR_DOWN] = self.map[i+1][j][self.DOOR_UP]
-                else:
-                    if(not self.three_close_door(i,j)):
-                        self.map[i][j][self.DOOR_DOWN] = self.random_status();
-                    else:
-                        self.map[i][j][self.DOOR_DOWN] = 1;
+    def generate_seq(self):
+        seq = []
+        i = 0
+        while i < 4:
+            j = randint(0, 3)
+            if not j in seq:
+                seq.append(j)
+                i += 1
+            else:
+                continue
+        print(seq)
+        return seq
 
-                if j == 0:
-                    self.map[i][j][self.DOOR_LEFT] = -1
-                elif self.map[i][j-1][self.DOOR_RIGHT] != 0:
-                    self.map[i][j][self.DOOR_LEFT] = self.map[i][j-1][self.DOOR_RIGHT]
-                else:
-                    if(not self.three_close_door(i,j)):
-                        self.map[i][j][self.DOOR_LEFT] = self.random_status();
-                    else:
-                        self.map[i][j][self.DOOR_LEFT] = 1;
+    def generate_up_door(self, i, j):
+        if i == 0:
+            self.map[i][j][self.DOOR_UP] = -1
+        elif self.map[i-1][j][self.DOOR_DOWN] != 0:
+            self.map[i][j][self.DOOR_UP] = self.map[i-1][j][self.DOOR_DOWN]
+        else:
+            if(not self.three_close_door(i,j)):
+                self.map[i][j][self.DOOR_UP] = self.random_status();
+            else:
+                self.map[i][j][self.DOOR_UP] = 1;
 
-                if j == self.col - 1:
-                    self.map[i][j][self.DOOR_RIGHT] = -1
-                elif self.map[i][j+1][self.DOOR_LEFT] != 0:
-                    self.map[i][j][self.DOOR_RIGHT] = self.map[i][j+1][self.DOOR_LEFT]
-                else:
-                    if(not self.three_close_door(i,j)):
-                        self.map[i][j][self.DOOR_RIGHT] = self.random_status();
-                    else:
-                        self.map[i][j][self.DOOR_RIGHT] = 1;
+    def generate_down_door(self, i, j):
+        if i == self.row - 1:
+            self.map[i][j][self.DOOR_DOWN] = -1
+        elif self.map[i+1][j][self.DOOR_UP] != 0:
+            self.map[i][j][self.DOOR_DOWN] = self.map[i+1][j][self.DOOR_UP]
+        else:
+            if(not self.three_close_door(i,j)):
+                self.map[i][j][self.DOOR_DOWN] = self.random_status();
+            else:
+                self.map[i][j][self.DOOR_DOWN] = 1;
+
+    def generate_left_door(self, i, j):
+        if j == 0:
+            self.map[i][j][self.DOOR_LEFT] = -1
+        elif self.map[i][j-1][self.DOOR_RIGHT] != 0:
+            self.map[i][j][self.DOOR_LEFT] = self.map[i][j-1][self.DOOR_RIGHT]
+        else:
+            if(not self.three_close_door(i,j)):
+                self.map[i][j][self.DOOR_LEFT] = self.random_status();
+            else:
+                self.map[i][j][self.DOOR_LEFT] = 1;
+
+    def generate_right_door(self, i, j):
+        if j == self.col - 1:
+            self.map[i][j][self.DOOR_RIGHT] = -1
+        elif self.map[i][j+1][self.DOOR_LEFT] != 0:
+            self.map[i][j][self.DOOR_RIGHT] = self.map[i][j+1][self.DOOR_LEFT]
+        else:
+            if(not self.three_close_door(i,j)):
+                self.map[i][j][self.DOOR_RIGHT] = self.random_status();
+            else:
+                self.map[i][j][self.DOOR_RIGHT] = 1;
 
     def print_map(self):
         print(self.map)
@@ -70,9 +95,9 @@ class Map:
             str = ' '
             for j in range(0, self.col):
                 if self.map[i][j][self.DOOR_UP] == -1:
-                    str += '_   '
+                    str += '_  '
                 else:
-                    str += '    '
+                    str += '   '
             print(str)
             str = ''
             for j in range(0, self.col):
@@ -85,18 +110,17 @@ class Map:
                     str += '|'
                 else:
                     str += ' '
-                str += ' '
             print(str)
             str = ' '
             for j in range(0, self.col):
                 if self.map[i][j][self.DOOR_DOWN] == -1:
-                    str += '_   '
+                    str += '_  '
                 else:
-                    str += '    '
+                    str += '   '
             print(str)
 
     def random_status(self):
-        i = randint(0, 2)
+        i = randint(0, 1)
         print(i)
         if(i == 0):
             return 1
